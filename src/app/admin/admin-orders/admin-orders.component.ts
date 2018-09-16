ecf473345097874aa0a05e16279575efd34dca19
ChamaraPW <pxdroid.cw@gmail.com>
2018-09-16 12:25:25 +0530

    Completed Basic funtionality
    completed last tow component my-orders and  mange orders

--- src/app/admin/admin-orders/admin-orders.component.ts
+++ src/app/admin/admin-orders/admin-orders.component.ts
@@ -1,3 +1,4 @@
+import { OrderService } from './../../services/order.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminOrdersComponent implements OnInit {
 
-  constructor() { }
+  orders$;
+
+  constructor(private orderService:OrderService) { }
 
   ngOnInit() {
+  this.orders$ =  this.orderService.getOrders();
   }
+
+
+
 
 }
--- src/app/my-order/my-order.component.ts
+++ src/app/my-order/my-order.component.ts
@@ -1,3 +1,6 @@
+import { switchMap } from 'rxjs/operators';
+import { OrderService } from './../services/order.service';
+import { AuthService } from './../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyOrderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService:AuthService,private orderService:OrderService) { }
+  orders$;
 
   ngOnInit() {
+    this.orders$ = this.authService.user$.pipe(switchMap(u => this.orderService.getOrdersByUser(u.uid)));
   }
 
 }
